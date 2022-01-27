3f5c3bad723a17903e60a74e79300de87dd1f2cd
Soniya141 <soniya.raichandani@knoldus.com>
2022-01-27 13:55:05 +0530

    add test suite collection

---
+++ Test Suites/Universal login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Universal login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>cf1fa149-77fb-47a3-8527-d234fda7f0a6</testSuiteGuid>
+</TestSuiteEntity>
--- Test Suites/Universal/TS-1.ts
+++ Test Suites/Universal/TS-1.ts
@@ -12,7 +12,7 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>c9becd62-070b-437f-a192-9dde2cdef181</testSuiteGuid>
    <testCaseLink>
-      <guid>abd03311-2493-4476-b639-a61c2315bb9a</guid>
+      <guid>b93d1aaf-3e3c-41a2-85c6-fd58f35c95ac</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Universal/Login/TC-1 Login with Moviestar</testCaseId>
@@ -27,6 +27,60 @@
          <type>DEFAULT</type>
          <value></value>
          <variableId>7b8c01bd-d1ed-460a-9c28-857ab1a504d7</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8d7dcc04-4614-498d-be6c-373fa27609b2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Universal/Login/TC-3 Login with Director</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>95fb0e14-5fef-4fda-bedb-a1303e9990d8</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>84e6614f-5623-4567-87f3-08512a37a8b0</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8d7276e9-5625-48e4-8e60-4177826d8b1c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Universal/Login/TC-2 Login with Screenwriter</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a1b99cef-9aa6-4138-a15f-091595ef2cf1</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0acab48c-8340-4dd2-864e-d2f33981b4ec</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fbaea44e-ca0a-4663-8e29-9f86818c9152</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Universal/Login/TC-4 Login with Extra</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>67b33b4f-2999-4ffd-8a70-765b60f1ca9c</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>a8f43b2d-1f27-484a-a740-5e374e46ce70</variableId>
       </variableLink>
    </testCaseLink>
 </TestSuiteEntity>
