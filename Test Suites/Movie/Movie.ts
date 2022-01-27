9620dc4fc8a1139ec8242f8e49e076af080597ab
Soniya141 <soniya.raichandani@knoldus.com>
2022-01-27 18:59:34 +0530

    updated test collectionss

--- Test Suites/Movie/Movie.ts
+++ Test Suites/Movie/Movie.ts
@@ -5,10 +5,16 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>01331af2-3499-475c-856e-c17e35f90f69</testSuiteGuid>
+   <testCaseLink>
+      <guid>6edce4d2-79bd-4ecd-a99e-1a5468fd787a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Universal/Tc-1 Verify the movies on movie ticket page</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
